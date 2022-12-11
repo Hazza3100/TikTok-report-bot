abee085c88240f091ef4cfa3f3aa7cc69a6368c6
Hazza <86918289+Hazza3100@users.noreply.github.com>
2022-12-11 11:45:44 +0000

    Added Id scraper.
    Added Id scraper.

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ class TikTok:
     def __init__(self) -> None:
         self.session  = requests.Session()
 
+    def get_id(self, username):
+        try:
+            with self.session as session:
+                headers = {'authority': 'www.tiktok.com','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','accept-language': 'en-US,en;q=0.9','cookie': 'msToken=cxfL0lxGWTg_UmhgLz8U_Nv3ecxsgvBu5OJ1FtmVgMd3cHWoFCxQnyHSUzoCzEMMh0XeZzSw_gjF8XhG8Qp9qiE7yi9Yjm5B64hK4qdEMnhOvQCK6bL2bP8h6pAAVdphB3w_yBje2nj3iFw=','sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"Windows"','sec-fetch-dest': 'document','sec-fetch-mode': 'navigate','sec-fetch-site': 'none','sec-fetch-user': '?1','upgrade-insecure-requests': '1','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',}
+
+                response = session.get(f'https://www.tiktok.com/@{username}', headers=headers)
+                return response.text.split('authorId":"')[1].split('","')[0]
+        except:
+            print(f"{Fore.BLUE}[ {Fore.RED}x {Fore.BLUE}]{Fore.RESET} Username Invalid")
+
+
     def report(self, userId, _proxy_):
         try:
             global reported
@@ -48,9 +59,12 @@ class TikTok:
 
 if __name__ == '__main__':
     os.system('cls')
-    userId  = input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] User ID {Fore.CYAN}> {Fore.WHITE}")
-    threads = int(input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] Threads {Fore.CYAN}> {Fore.WHITE}"))
-    proxy   = input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] Use proxies (y/n) {Fore.CYAN}> {Fore.WHITE}")
+    # userId  = input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] User ID {Fore.CYAN}> {Fore.WHITE}")
+    username  = input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] Username {Fore.CYAN}> {Fore.WHITE}")
+    threads   = int(input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] Threads {Fore.CYAN}> {Fore.WHITE}"))
+    proxy     = input(f"{Fore.GREEN}[{Fore.CYAN} ? {Fore.GREEN}] Use proxies (y/n) {Fore.CYAN}> {Fore.WHITE}")
+    userId    = TikTok().get_id(username)
+    
     if proxy == "y":
         _proxy_ = "Use"
         for i in range(threads):
